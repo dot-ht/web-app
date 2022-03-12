d92545e30f18583eea0a9522e817e2333a5e157c
Kaloyan Doychinov <kokolia1070@gmail.com>
2022-03-12 14:48:14 +0200

    [FIX] Chat Parse

--- src/components/Chat/ChatMessage.jsx
+++ src/components/Chat/ChatMessage.jsx
@@ -1,10 +1,9 @@
 const Chat = ({key, sender, message}) => {
     let classN = "flex justify-"
-    classN += sender ? "end" : "start";
-    console.log(message);
+    classN += (sender) ? "end" : "start";
     return(
         <li className={classN}>
-        <div className="relative max-w-xl px-4 py-2 text-gray-700 rounded shadow">
+        <div className="relative max-w-xl px-4 py-2 rounded shadow text-gray-700 dark:text-white bg-slate-800">
           <span className="block">{message}</span>
         </div>
       </li>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,43 +5,19 @@ import PropTypes from 'prop-types';
 
 const Navigation = () => (
     <nav className="flex items-center justify-between max-w-3xl p-4 mx-auto dark:bg-slate-900">
-    <a
+    <Link
         className="inline-flex items-center justify-center w-10 h-10 bg-gray-100 rounded-lg"
-        href="/"
-    >
+        to="/"
+    ></Link>
         
-    </a>
-
     <ul className="flex items-center space-x-2 text-sm font-medium text-gray-500">
         <li className="hidden lg:block">
-        <a className="px-3 py-2 rounded-lg" href="/"> Home </a>
+        <Link className="px-3 py-2 rounded-lg" to="/"> Home </Link>
         </li>
-
+        <li><Link to="/chat" className="px-3 py-2 rounded-lg">Chat</Link></li>
         <li><Link to="/planets" className="px-3 py-2 rounded-lg">Planets</Link></li>
-
-        <li>
-        <a
-            className="inline-flex items-center px-3 py-2 rounded-lg"
-            href=""
-            target="_blank"
-        >
-            External
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="ml-1.5 w-4 h-4"
-            >
-            <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-            ></path>
-            </svg>
-        </a>
-        </li>
+        <li><Link to="/planets" className="px-3 py-2 rounded-lg">Settings</Link></li>
+        <li><Link to="/planets" className="px-3 py-2 rounded-lg">About us</Link></li>
     </ul>
     </nav>
 );
